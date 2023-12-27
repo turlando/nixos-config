add28b323b23ef811b137d3e6f8c00073c85a0d4
Tancredi Orlando <tancredi.orlando@gmail.com>
2023-12-27 10:59:39 +0000

    Update antigone boot configuration

--- hosts/antigone/boot.nix
+++ hosts/antigone/boot.nix
@@ -1,5 +1,8 @@
-{ ... }:
+{ config, lib, ... }:
 
+let
+  inherit (lib.files) readSshKey;
+in
 {
   boot.drives = [
     /dev/disk/by-id/ata-Lexar_SSD_NS100_512GB_MJ95272016149
@@ -16,5 +19,23 @@
   boot.ephemeral = {
     enable = true;
     dataset = "system/root@empty";
+  };
+
+  boot.initrd.kernelModules = [ "r8169" ];
+
+  boot.initrd.network.enable = true;
+
+  boot.initrd.network.ssh = let
+    stateDir = config.boot.ephemeral.stateDir;
+    hostKeys = [
+      (stateDir + "/etc/ssh-initrd/ssh_host_rsa_key")
+      (stateDir + "/etc/ssh-initrd/ssh_host_ed25519_key")
+    ];
+    authorizedKeys = [ (readSshKey "boot") ];
+  in {
+    enable = true;
+    port = 2222;
+    hostKeys = hostKeys;
+    authorizedKeys = authorizedKeys;
   };
 }
