b0e00fe66bce450628d4a397332a1e496b7cb0a9
Tancredi Orlando <tancredi.orlando@gmail.com>
2023-12-27 10:35:57 +0000

    Add files helpers

--- lib/default.nix
+++ lib/default.nix
@@ -2,6 +2,7 @@ lib:
 
 {
   attrsets = lib.attrsets // (import ./attrsets.nix lib);
+  files = import ./files.nix lib;
   filesystems = import ./filesystems.nix lib;
   grub = import ./grub.nix lib;
 }
---
+++ lib/files.nix
@@ -0,0 +1,18 @@
+lib:
+
+rec {
+  # type: path -> path
+  getFile = path: dirOf <nixos-config> + /files + path;
+
+  # type: path -> string
+  readFile = path: builtins.readFile (getFile path);
+
+  # type: string -> path
+  getSshKey = x: getFile (/ssh-keys + "/${x}.pub");
+
+  # type: string -> string
+  readSshKey = x: builtins.readFile (getSshKey x);
+
+  # type: string -> string
+  readPassword = x: readFile (/hashed-passwords + "/${x}.txt");
+}
