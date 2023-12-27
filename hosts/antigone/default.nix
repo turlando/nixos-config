77d2d2d7395ee966b61140ed557a6a0c0b92abfe
Tancredi Orlando <tancredi.orlando@gmail.com>
2023-12-27 11:02:11 +0000

    Add antigone network configuration

--- hosts/antigone/default.nix
+++ hosts/antigone/default.nix
@@ -4,6 +4,7 @@
   imports = [
     ./hardware.nix
     ./boot.nix
+    ./networking.nix
   ];
 
   system.stateVersion = "23.11";
---
+++ hosts/antigone/networking.nix
@@ -0,0 +1,18 @@
+{ ... }:
+
+{
+  networking.hostName = "antigone";
+  networking.hostId = "4d86c32a"; # Required by ZFS
+
+  networking.interfaces.eth0 = {
+    macAddress = "f4:6d:04:7b:d3:0e";
+    useDHCP = true;
+  };
+
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    permitRootLogin = "prohibit-password";
+    forwardX11 = true;
+  };
+}
