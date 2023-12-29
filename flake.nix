f4c1697ae90207d6b0960c36cebceaf09b479fa0
Tancredi Orlando <tancredi.orlando@gmail.com>
2023-12-29 11:10:46 +0000

    Remove unnecessary indentation

--- flake.nix
+++ flake.nix
@@ -5,20 +5,19 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs:
-    let 
-      lib = nixpkgs.lib.extend (final: prev: import ./lib prev);
-    in {
-      packages = {
-        "x86_64-linux" = import ./pkgs nixpkgs.legacyPackages."x86_64-linux";
-      };
+  outputs = { self, nixpkgs, ... }@inputs: let 
+    lib = nixpkgs.lib.extend (final: prev: import ./lib prev);
+  in {
+    packages = {
+      "x86_64-linux" = import ./pkgs nixpkgs.legacyPackages."x86_64-linux";
+    };
 
-      nixosConfigurations = {
-        antigone  = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          specialArgs = { inherit lib; inherit (self) packages; };
-          modules = [ ./nixos ./hosts/antigone ];
-        };
+    nixosConfigurations = {
+      antigone  = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit lib; inherit (self) packages; };
+        modules = [ ./nixos ./hosts/antigone ];
       };
     };
+  };
 }
