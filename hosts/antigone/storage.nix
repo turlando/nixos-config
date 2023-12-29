e7088481a4473cf57255dc591ad98c94ffd6f25d
Tancredi Orlando <tancredi.orlando@gmail.com>
2023-12-29 10:37:13 +0000

    Complete antigone storage config migration

--- hosts/antigone/storage.nix
+++ hosts/antigone/storage.nix
@@ -1,6 +1,13 @@
-{ config, ... }:
+{ config, lib, ... }:
 
+let
+  inherit (lib.attrsets) mergeAttrsets;
+in
 {
+  users.groups.storage = {
+    gid = 5000;
+  };
+
   storage.pools = {
     system = {
       datasets = {
@@ -35,5 +42,78 @@
       };
     };
     backup = { datasets = { }; };
+  };
+
+  services.zfs.autoScrub = {
+    enable = true;
+    # Run on the first Monday of every month at 02:00.
+    interval = "Mon *-*-1..7 02:00:00";
+  };
+
+  services.zfs.trim = {
+    enable = true;
+    # Run on every Friday at 02:00.
+    interval = "Fri *-*-* 02:00:00";
+  };
+
+  services.sanoid = {
+    enable = true;
+    extraArgs = [ "--verbose" ];
+    datasets = let
+      systemDatasets = config.storage.pools.system.datasets;
+      storageDatasets = config.storage.pools.storage.datasets;
+      backupPool = config.storage.pools.backup;
+      dailyCfg = {
+        yearly = 0; monthly = 0; daily = 30; hourly = 0; frequently = 0;
+        autosnap = true; autoprune = true; recursive = false;
+      };
+      backupCfg = {
+        yearly = 0; monthly = 12; daily = 30; hourly = 48;
+        autosnap = false; autoprune = true; recursive = true;
+      };
+    in {
+      # The system state changes so slowly I manually snapshot it.
+      # "${systemDatasets."state".name}" = dailyCfg;
+      "${systemDatasets."services/quassel".name}" = dailyCfg;
+      "${storageDatasets."books".name}" = dailyCfg;
+      "${storageDatasets."papers".name}" = dailyCfg;
+      "${storageDatasets."music/electronic".name}" = dailyCfg;
+      "${backupPool.name}" = backupCfg;
+    };
+  };
+
+  services.syncoid = {
+    enable = true;
+    commands = let
+      systemDatasets = config.storage.pools.system.datasets;
+      storageDatasets = config.storage.pools.storage.datasets;
+
+      common = {
+        extraArgs = [ "--no-sync-snap" "--no-resume" ];
+        sendOptions = "Rw";
+        recursive = false;
+        localTargetAllow = [
+          "change-key" "compression" "create" "mount" "mountpoint"
+          "receive" "rollback" "acltype"
+        ];
+      };
+
+      # type: str -> str -> str
+      # Example:
+      #  addPrefix "system/services/quassel"
+      #  => "backup/system/services/quassel"
+      addBackupPrefix = dataset:
+        config.storage.pools.backup.name + "/" + dataset;
+
+      command = dataset: args:
+        { "${dataset.name}" =
+            { target = addBackupPrefix dataset.name; } // args; };
+    in mergeAttrsets [
+      (command systemDatasets."state" common)
+      (command systemDatasets."services/quassel" common)
+      (command storageDatasets."books" common)
+      (command storageDatasets."papers" common)
+      (command storageDatasets."music/electronic" common)
+    ];
   };
 }
