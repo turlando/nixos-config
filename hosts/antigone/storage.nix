4a7551df3069d1f78777b25f59601a2f6648a396
Tancredi Orlando <tancredi.orlando@gmail.com>
2023-12-29 10:12:37 +0000

    Add storage, scratch, backup pools to antigone config

--- hosts/antigone/storage.nix
+++ hosts/antigone/storage.nix
@@ -16,5 +16,24 @@
         "home/tancredi" = { mountPoint = "/home/tancredi"; };
       };
     };
+    storage = {
+      datasets = {
+        "books" = { mountPoint = "/mnt/storage/books"; };
+        "papers" = { mountPoint = "/mnt/storage/papers"; };
+        "music" = { mountPoint = null; };
+        "music/electronic" = { mountPoint = "/mnt/storage/music/electronic"; };
+      };
+    };
+    scratch = {
+      datasets = {
+        "downloads" = { mountPoint = null; };
+        "downloads/slskd" = { mountPoint = null; };
+        "music-mp3" = { mountPoint = null; };
+        "music-mp3/electronic" = { mountPoint = "/mnt/scratch/music-mp3/electronic"; };
+        "music-opus" = { mountPoint = null; };
+        "music-opus/electronic" = { mountPoint = "/mnt/scratch/music-opus/electronic"; };
+      };
+    };
+    backup = { datasets = { }; };
   };
 }
