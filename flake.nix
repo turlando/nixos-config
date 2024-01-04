d5abd97f01fb4628d72ffe44671b4e157622c368
Tancredi Orlando <tancredi.orlando@gmail.com>
2024-01-04 17:33:55 +0000

    Remove emacs-overlay

--- flake.nix
+++ flake.nix
@@ -1,23 +1,10 @@
 {
   description = "NixOS configuration";
 
-  nixConfig = {
-    extra-trusted-substituters = [
-      "https://nix-community.cachix.org"
-    ];
-    extra-trusted-substituers-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" 
-    ];
-  };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    emacs-overlay = {
-      url = "github:nix-community/emacs-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
@@ -25,21 +12,21 @@
   outputs = { self, nixpkgs, home-manager, ... }@inputs: let 
     system = "x86_64-linux";
     lib = nixpkgs.lib.extend (final: prev: import ./lib prev);
-    overlays = with inputs; [ emacs-overlay.overlays.default ];
+    pkgs = nixpkgs.legacyPackages."${system}";
+    packages = self.packages."${system}";
   in {
     packages = {
-      "${system}" = import ./pkgs (import nixpkgs { inherit system; });
+      "${system}" = import ./pkgs pkgs;
     };
 
     nixosConfigurations = {
       antigone  = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
+        inherit system;
         specialArgs = {
           inherit lib;
-          packages = self.packages."${system}";
+          inherit packages;
         };
         modules = [
-          ({ ... }: { nixpkgs.overlays = overlays; })
           ./nixos
           ./hosts/common
           ./hosts/antigone
@@ -49,7 +36,7 @@
 
     homeConfigurations = {
       "tancredi@antigone" = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs { inherit system; inherit overlays; };
+        inherit pkgs;
         modules = [
           ./users/tancredi-common
           ./users/tancredi-antigone
