648296c3799d18600c4e9edf63b6ddff30c2fd08
Tancredi Orlando <tancredi.orlando@gmail.com>
2024-01-09 10:45:40 +0100

    (Partially) manage Firefox config with home-manager

--- users/tancredi-ifigenia/packages.nix
+++ users/tancredi-ifigenia/packages.nix
@@ -3,9 +3,21 @@
 {
   programs.emacs.package = pkgs.emacs29-pgtk;
 
+  programs.firefox = {
+    enable = true;
+    profiles.tancredi = {
+      isDefault = true;
+      settings = {
+        "extensions.pocket.enabled" = false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+      };
+    };
+  };
+
   home.packages = with pkgs; [
-    firefox
     source-code-pro
+
     telegram-desktop
+    keepassxc
   ];
 }
