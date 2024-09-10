03f048254ec73ec559fbd376edd2bed78c19b778
Tancredi Orlando <tancredi.orlando@gmail.com>
2024-09-10 17:25:34 +0200

    Enable silent boot on ifigenia

--- hosts/ifigenia/configuration.nix
+++ hosts/ifigenia/configuration.nix
@@ -43,6 +43,19 @@
 
   boot.plymouth.enable = true;
 
+  # Enable "Silent Boot"
+  boot.consoleLogLevel = 0;
+  boot.initrd.verbose = false;
+  boot.kernelParams = [
+    "quiet"
+    "splash"
+    "boot.shell_on_fail"
+    "loglevel=3"
+    "rd.systemd.show_status=false"
+    "rd.udev.log_level=3"
+    "udev.log_priority=3"
+  ];
+
   boot.supportedFilesystems = [ "ntfs" ];
 
   zramSwap = {
