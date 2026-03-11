0593cd651dbc504ffb78ceb1a3688709a187f4ff
Tancredi Orlando <tancredi.orlando@gmail.com>
2026-03-11 13:50:13 +0100

    chore: Update lock file

--- nixos/profiles/zfs.nix
+++ nixos/profiles/zfs.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-  boot.kernelPackages = pkgs.linuxPackages_6_18;
+  boot.kernelPackages = pkgs.linuxPackages_6_19;
   boot.zfs.package = pkgs.zfs_2_4;
 
   services.zfs.autoScrub.enable = true;
