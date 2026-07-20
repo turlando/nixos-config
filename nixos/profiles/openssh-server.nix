61601832b0c6d5f9f92a6be4eaf3a9529b490163
Tancredi Orlando <tancredi.orlando@gmail.com>
2026-07-20 17:50:15 +0200

    feat(openssh-server): Install client terminfo on SSH hosts

--- nixos/profiles/openssh-server.nix
+++ nixos/profiles/openssh-server.nix
@@ -1,6 +1,9 @@
-_:
+{ pkgs, ... }:
 {
   environment.persistence.paths = [ "/etc/ssh" ];
+
+  # Terminfo for the terminals we connect from (ghostty sets TERM=xterm-ghostty).
+  environment.systemPackages = [ pkgs.ghostty.terminfo ];
 
   services.openssh = {
     enable = true;
