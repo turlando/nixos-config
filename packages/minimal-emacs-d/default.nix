f53f7d654d2c44dc95e78118393aa03b89e2bfed
Tancredi Orlando <tancredi.orlando@gmail.com>
2026-08-04 09:30:45 +0200

    chore(minimal-emacs-d): Bump to 1.5.1

--- packages/minimal-emacs-d/default.nix
+++ packages/minimal-emacs-d/default.nix
@@ -3,6 +3,6 @@
 fetchFromGitHub {
   owner = "jamescherti";
   repo = "minimal-emacs.d";
-  rev = "1.5.0";
-  hash = "sha256-CuwxuAolXozZdi/HJwszQI21k3a2XApI2oqCsaiTZPo=";
+  rev = "1.5.1";
+  hash = "sha256-i1gVkDR8zB8L7M67iw5atF9tb+6hlDLYg9RA05U0gS8=";
 }
